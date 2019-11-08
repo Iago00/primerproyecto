478aebeccd5fc61f2ffeebd436c8a34955c2baba
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2019-11-08 14:23:16 +0100

    jibh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("cho");
+        System.out.println("chooo");
         //todo
         //comprobar q el numero es negativo
 
