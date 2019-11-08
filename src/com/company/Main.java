26cca94e39c6b06dd75a5a697d068547b84fb662
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2019-11-08 14:21:42 +0100

    salida mensaje

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hola");
+        System.out.println("chao");
+        //todo
+        //comprobar q el numero es negativo
+
     }
 }
